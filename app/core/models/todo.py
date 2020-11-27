060d91465bf003aa93a39a043868e6b5a9369e75
Vinh Le <lethanhvinh95@gmail.com>
2020-11-27 06:53:04 +0200

    chore: remove .idea dir from git

--- app/core/models/todo.py
+++ app/core/models/todo.py
@@ -4,12 +4,12 @@ from .base import BaseModel
 
 
 class Todo(BaseModel):
-  """Todo model"""
-  title = models.CharField(max_length=255)
-  description = models.CharField(max_length=255, blank=True)
-  completed = models.BooleanField(default=False)
-  completed_at = models.DateTimeField(default=None, null=True)
-  completed_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=None, null=True)
+    """Todo model"""
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(default=None, null=True)
+    completed_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=None, null=True)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
